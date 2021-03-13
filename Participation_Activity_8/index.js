3c71da115b5db1f2a93446eafb40a86c3da5ed8b
Luis Lopez <llopez103@knights.ucf.edu>
2021-03-12 19:33:02 -0500

    Participation Activity 8 is complete!

--- Participation_Activity_8/index.js
+++ Participation_Activity_8/index.js
@@ -1,1 +1,31 @@
 import Express from 'express';
+const App=Express();
+const port = 45030;
+
+
+App.get("/user/:id",
+    (req,res)=>{
+        
+        res.json({userid:req.params.id});
+
+        });
+App.get("/user/",
+    (req,res)=>{
+        
+        res.send("Content Goes Here! To see JSON go to: <a href=\"http://localhost:45030/user/id\">http://localhost:45030/user/id</a>");
+
+        });
+
+App.get("/",
+    (req,res)=>{
+        
+        res.send(
+        "Try going to <br><br><a href=\"http://localhost:45030/user\">http://localhost:45030/user</a><br><br>and <br><br><a href=\"http://localhost:45030/user/id\">http://localhost:45030/user/id</a>");
+        });
+
+
+App.listen(port,
+    ()=>{
+        
+    
+        });
