9ff50ffdefd3e10e323af7faee672ecd3d4eebc0
Luis Lopez <llopez103@knights.ucf.edu>
2021-03-19 22:45:50 -0400

    Lab 8 is Complete

--- lab8/index.js
+++ lab8/index.js
@@ -4,9 +4,32 @@ import fs from "fs";
 const express= Express();
 const port = 3010;
 
+let databaseContents=fs.readFileSync("database.json");
+let database=JSON.parse(databaseContents);
 
 express.listen(port,()=>{
-    
     console.log("Express Server is Running on Port 3010, localhost/3010 ;");
+})
+
+
+express.get("/employees/:name",(req,res)=>{
+    let employees = {"Error" : "Not Found"};
     
+    database.forEach((value)=>{
+        if(req.params.name == value.name){
+            employees = value;
+        }
+    });
+    res.json(employees);
+})
+
+express.get("/ages/:number",(req,res)=>{
+    let ages = {"Error" : "Not Found"};
+    
+    database.forEach((value)=>{
+        if(req.params.number == value.age){
+            ages = value;
+        }
+    });
+    res.json(ages);
 })
