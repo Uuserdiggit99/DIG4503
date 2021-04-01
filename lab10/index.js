97d13a02ecd3367be6d72709f14a1fa36dd3328c
Luis Lopez <llopez103@knights.ucf.edu>
2021-04-01 00:58:39 -0400

    First Commit for Lab 10

---
+++ lab10/index.js
@@ -0,0 +1,55 @@
+import Express from "express";
+import fs from "fs";
+
+const App= Express();
+const port = 3010;
+
+let databaseContents=fs.readFileSync("database.json");
+let database=JSON.parse(databaseContents);
+
+App.use("/", Express.static("public"));
+
+App.listen(port,()=>{
+    console.log("Express Server is Running on Port 3010, localhost/3010 ;");
+})
+
+
+App.get("/employees/:name",(req,res)=>{
+    let employees = {"Error" : "Not Found"};
+    
+    database.forEach((value)=>{
+        if(req.params.name == value.name){
+            employees = value;
+        }
+    });
+    res.json(employees);
+})
+
+App.get("/ages/:number",(req,res)=>{
+    let ages = {"Error" : "Not Found"};
+    
+    database.forEach((value)=>{
+        if(req.params.number == value.age){
+            ages = value;
+        }
+    });
+    res.json(ages);
+})
+
+App.post("/employees/:name/:age",(req,res)=>{
+    
+    let result = {
+        "name": req.params.name,
+        "age": parseInt(req.params.age)
+    };//let results close
+
+    database.push(result);
+    
+
+    fs.writeFileSync("database.json", JSON.stringify(database, null, '\t'));
+    
+    res.json(result);
+  
+});//App.Post Close
+
+
