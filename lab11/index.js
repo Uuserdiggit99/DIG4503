4f6e0be40623c78e6c58c2ee594d8a4ab614393d
Luis Lopez <llopez103@knights.ucf.edu>
2021-04-08 22:19:47 -0400

    lab11 is complete, Should print reslts matching: { beds : {:5}, price : {: 200}, review_scores.review_scores_rating : {:99}  }

--- lab11/index.js
+++ lab11/index.js
@@ -10,18 +10,16 @@ MongoClient.connect(URL, {useUnifiedTopology: true})
     
     let collection=database.collection("listingsAndReviews");
     
-    //let cursor=collection.find({ beds : {$gte:5} }, { price : {$lte: 200} }, {"review_scores.review_scores_rating" : {$gte:99} } );
+    let cursor=collection.find({ beds : {$gte:5}, price : {$lte: 200}, "review_scores.review_scores_rating" : {$gte:99}  } );
     
-    let cursor=collection.find({ beds : {$gte:5} }, { price : {$lte: 200} });
-    
-    cursor.forEach(document=> {
-        
-        console.log(document.name);
-        
-    }, ()=>{
-        connection.close();
-        console.log("\nDisconnected\n");
-    })
+     cursor.forEach(document=> {
+         
+         console.log(document.name);
+         
+     }, ()=>{
+         connection.close();
+         console.log("\nDisconnected\n");
+     })
     
 })
 .catch(error=>{
